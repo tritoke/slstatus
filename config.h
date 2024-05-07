3fee0c69a236eb0be0ce331d3f9c1737bb5fbaab
tritoke <tritoke@protonmail.com>
2024-05-07 19:54:12 +0100

    ✨ truncated sensors output

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static const struct arg args[] = {
 	/* CPU info */
 	{ cpu_perc, "CPU: %s%%", NULL},
 	{ cpu_freq, " %sHz", NULL},
-	{ run_command, " %s°C" SEP, "sensors -j coretemp-isa-0000 | jq '.\"coretemp-isa-0000\".\"Package id 0\".temp1_input'" },
+	{ run_command, " %s°C" SEP, "sensors -j coretemp-isa-0000 | jq '.\"coretemp-isa-0000\".\"Package id 0\".temp1_input' | cut -d. -f1" },
 
 	/* RAM info */
 	{ ram_used, "RAM: %s" SEP, NULL },
